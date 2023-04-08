16301faf8d45fc592a1a87f3be0c1ef216b295a2
ozguner00 <ozguner00@gmail.com>
2023-04-08 06:55:43 +0300

    Main Thread yerine 4 Thread uzerinde calisir hale getirildi.

--- src/ThreadlerinYarisi/Main.java
+++ src/ThreadlerinYarisi/Main.java
@@ -3,9 +3,11 @@ package ThreadlerinYarisi;
 import java.util.ArrayList;
 
 public class Main {
+
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<Integer>();
         ArrayList<ArrayList<Integer>> parts = new ArrayList<ArrayList<Integer>>();
+
 
         for(int i = 1; i <10001; i++ ){
             numbers.add(i);
@@ -15,17 +17,20 @@ public class Main {
             parts.add(part);
         }
 
-        Threads t1 = new Threads(parts.get(0));
-        Threads t2 = new Threads(parts.get(1));
-        Threads t3 = new Threads(parts.get(2));
-        Threads t4 = new Threads(parts.get(3));
-        t1.run();
-        t2.run();
-        t3.run();
-        t4.run();
+        Threads t1 = new Threads(parts.get(0),0);
+        Threads t2 = new Threads(parts.get(1),1);
+        Threads t3 = new Threads(parts.get(2),2);
+        Threads t4 = new Threads(parts.get(3),3);
 
-        System.out.println(t1.getEvenNum());
-        System.out.println(t1.getOddNum());
+        Thread s1 = new Thread(t1);
+        Thread s2 = new Thread(t2);
+        Thread s3 = new Thread(t3);
+        Thread s4 = new Thread(t4);
+
+        s1.start();
+        s2.start();
+        s3.start();
+        s4.start();
 
     }
 }
--- src/ThreadlerinYarisi/Threads.java
+++ src/ThreadlerinYarisi/Threads.java
@@ -6,20 +6,34 @@ public class Threads implements Runnable {
     private ArrayList<Integer> arr;
     static private ArrayList<Integer> oddNum = new ArrayList<>();
     static private ArrayList<Integer> evenNum = new ArrayList<>();
-
-    public Threads(ArrayList<Integer> arr) {
+    private int sleepTime; //Threadleri sırayla çalıştırabilmek için.
+    public Threads(ArrayList<Integer> arr,int sleepTime) {
         this.arr = arr;
+        this.sleepTime = sleepTime;
     }
 
     @Override
     public void run() {
-         for(int i = 0; i < arr.size(); i++){
-             if(i%2 == 0){
-                 evenNum.add(arr.get(i));
-             }else {
-                 oddNum.add(arr.get(i));
-             }
-         }
+        try {
+            Thread.sleep(this.sleepTime);
+            //System.out.println("Aktif Thread :" + Thread.currentThread().getName());
+            synchronized(oddNum){//1 Thread oddNum ile çalışmaya başladığı zaman oddNum listesini kullanmaya devam ettiği müddetçe diğer Thread başlayamıyor.
+                for(int i = 0; i < this.arr.size(); i++){
+                    if(i%2 == 0){
+                        evenNum.add(this.arr.get(i));
+                    }else {
+                        oddNum.add(this.arr.get(i));
+                    }
+                }
+            }
+            if(oddNum.size() > 3751){ //Son thread durduğu zaman diziyi yazdırmak için eklendi.
+                System.out.println(getEvenNum());
+                System.out.println(getOddNum());
+            }
+        }catch (Exception e){
+            System.out.println("Error Message :"  + e.getMessage());
+        }
+
     }
 
     public ArrayList<Integer> getOddNum() {
